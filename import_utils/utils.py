8e352179c56dfdfe6a87d427a7e65af5f3da8aa6
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-05-15 09:06:18 +0200

    places where the dead lived imported

--- import_utils/utils.py
+++ import_utils/utils.py
@@ -1,9 +1,50 @@
 import re
 import pandas as pd
+from entities.models import *
+from relations.models import *
+from vocabularies.models import *
+
+
+def clean_place_string(place_string):
+
+    """ takes e.g. 'St. Lorenzen (St. Michaelsburg)' and
+    returns the tuple ('St. Lorenzen', 'St Michaelsburg') """
+
+    first_place = place_string.split("(")[0]
+    lg = " ".join(place_string.split("(")[1:])
+    lg = lg.replace(')', '')
+    return(first_place, lg)
+
+
+def create_places_from_place_string(place_string):
+
+    """ takes e.g. 'St. Lorenzen (St. Michaelsburg)' creates two place objects,
+    relates them 'part_of' and returns a tuple containing those two places."""
+
+    cleaned = clean_place_string(place_string)
+    first_second, _ = PlacePlaceRelation.objects.get_or_create(
+        name="part of",
+        name_reverse="has part"
+    )
+    if len(cleaned) == 2:
+        first, _ = Place.objects.get_or_create(
+            name=cleaned[0]
+        )
+        second, _ = Place.objects.get_or_create(
+            name=cleaned[1]
+        )
+        relate_places = PlacePlace.objects.get_or_create(
+            relation_type=first_second,
+            related_placeA=first,
+            related_placeB=second
+        )
+        return (first, second)
 
 
 def get_filtered_sheets(file, filterstring='V'):
+
     """ returns a list of sheet names containing the filterstring value """
+
     sheets = pd.read_excel(file, None).keys()
     filtered = [x for x in sheets if x.startswith(filterstring)]
     return {'sheets': sheets, 'filtered': filtered}
